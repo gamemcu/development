c298b10910de80b0184123e695b173d8e46f3d43
regocxy <regocxy@163.com>
2017-08-17 20:38:38 +0800

    update

--- games/fun/test.lua
+++ games/fun/test.lua
@@ -1,35 +1,25 @@
 gpio.config({
-	gpio={12,14},
-	dir=gpio.OUT,
-	opendrain=0,
-	pull=gpio.PULL_UP   
+    gpio={32,33,25,26},
+    dir=gpio.IN,
+    opendrain=0,
+    pull=gpio.INTR_UP   
 })
-
-gpio.config({
-	gpio={25,26,27,32,33},
-	dir=gpio.IN,
-	opendrain=0,
-	pull=gpio.INTR_UP   
-})
-
-gpio.write(12,0)
-gpio.write(14,1)
 
 adc.setup(1,6,adc.ATTEN_11db)
 adc.setup(1,7,adc.ATTEN_11db)
+ws2812.write({pin = 4, data = string.char(255, 0, 0, 255, 255, 255)})
 
-local function adcloop()
+local function testloop()
 	print('x',adc.read(1,6))
 	print('y',adc.read(1,7))
-	print('k1',gpio.read(25))
-	print('k2',gpio.read(26))
-	print('k3',gpio.read(27))
-	print('k4',gpio.read(32))
-	print('k5',gpio.read(33))
+	print('k1',gpio.read(32))
+	print('k2',gpio.read(33))
+	print('k3',gpio.read(25))
+	print('k4',gpio.read(26))
 	print()
-	loop_tmr1:start()
+	test_loop_tmr1:start()
 end
 
-loop_tmr1 = tmr.create()
-loop_tmr1:register(1000, tmr.ALARM_SEMI, adcloop)
-loop_tmr1:start()
+test_loop_tmr1 = tmr.create()
+test_loop_tmr1:register(1000, tmr.ALARM_SEMI, testloop)
+test_loop_tmr1:start()
