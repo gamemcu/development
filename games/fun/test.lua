302d01ea80010d3ef6d33c01381c0193fd051876
regocxy <regocxy@163.com>
2017-08-19 18:17:17 +0800

    add tools

--- games/fun/test.lua
+++ games/fun/test.lua
@@ -1,5 +1,5 @@
 gpio.config({
-    gpio={32,33,25,26},
+    gpio={32,33,26,27},
     dir=gpio.IN,
     opendrain=0,
     pull=gpio.INTR_UP   
@@ -8,18 +8,34 @@ gpio.config({
 adc.setup(1,6,adc.ATTEN_11db)
 adc.setup(1,7,adc.ATTEN_11db)
 ws2812.write({pin = 4, data = string.char(255, 0, 0, 255, 255, 255)})
+-- ledc.setup(25,0,0,0,0,1000)
+yaw, pitch, roll = 0, 0, 0
+local function mpu6050_init()
+	i2c.setup(i2c.HW0, 17, 16, i2c.FAST)
+	gpio.trig(21, gpio.INTR_UP, function(gpio, level)
+		yaw, pitch, roll = mpu6050dmp.trans()
+	end)
+	mpu6050dmp.setup(i2c.HW0)
+	mpu6050dmp.set_xgyro_offset(220)
+	mpu6050dmp.set_ygyro_offset(76)
+	mpu6050dmp.set_zgyro_offset(-85)
+	mpu6050dmp.set_zaccel_offset(1788)
+	mpu6050dmp.enable(1)
+end
 
 local function testloop()
 	print('x',adc.read(1,6))
 	print('y',adc.read(1,7))
 	print('k1',gpio.read(32))
 	print('k2',gpio.read(33))
-	print('k3',gpio.read(25))
-	print('k4',gpio.read(26))
+	print('k3',gpio.read(26))
+	print('k4',gpio.read(27))
+	print('yaw',yaw,'pitch',pitch,'roll',roll)
 	print()
 	test_loop_tmr1:start()
 end
-
+-- pcall(mpu6050_init)
+mpu6050_init()
 test_loop_tmr1 = tmr.create()
 test_loop_tmr1:register(1000, tmr.ALARM_SEMI, testloop)
 test_loop_tmr1:start()
---
+++ tools/img2oled.py
@@ -0,0 +1,153 @@
+import sys
+import argparse
+import Image,ImageDraw,glob,os,re
+from os.path import basename,splitext
+
+def getsize(name,w,h):
+    r=re.findall(r'(\d+)x(\d+).',name) or [(w,h)]
+    return int(r[0][0]),int(r[0][1])
+
+def gencode(name,frames,w,h,data):
+    s = str.format('%s = string.char(0x%02x,0x%02x,0x%02x,\n'%(name,ew,eh,frames))
+    rlen = len(data)
+    line = 0
+    for i in range(0, rlen):
+        s += str.format("0x%02x," %data[i])
+        if i == rlen-1:
+            break
+        if i % 12 ==11:
+            line += 1
+            if line == 8:
+                line = 0
+                s = s[0:len(s)-1]
+                s += ')..string.char(\n'
+            else:
+                s += '\n'
+    s = s[0:len(s)-1]
+    s += ')\n\n'
+    return s
+
+def draw(w,h,data):
+    k=0
+    for y in range(h):
+        for x in range(w):
+            if data[k+x][0]:
+                print('O'),
+            else:  
+                print('.'),
+        k+=w
+        print
+
+def decode0(w,h,data):
+    v=0
+    ret=[]
+    for y in range(h):
+        for x in range(w):
+            xx=x&7
+            if data[y*w+x]>0:
+                v|=1<<xx
+            if (xx==7) or (x==w-1):
+                ret.append(v)
+                v=0
+    return ret
+
+def decode1(w,h,data):
+    ret=[]
+    y=0
+    imax=8
+    while y<h:
+        k=y*w
+        if y+8>h:
+            imax=h&7
+        for x in range(w):
+            kk=k
+            v=0
+            for i in range(imax):
+                if (data[kk+x][3] == 255) and (data[kk+x][0] == 255):
+                    v|=1<<i
+                kk+=w
+            ret.append(v)
+        y+=8
+    return ret
+
+def decode2(w,h,data):
+    ret=[]
+    y=0
+    imax=8
+    while y<h:
+        k=y*w
+        if y+8>h:
+            imax=y&7
+        for x in range(w):
+            kk=k
+            v=0
+            v1=0xff
+            for i in range(imax):
+                if (data[kk+x][3] == 255) and (data[kk+x][0] == 255):
+                    v|=(data[k+x][0]&1)<<i
+                    v1&=~(1<<i)
+                kk+=w
+            ret.append(v)
+            ret.append(v1)
+        y+=8
+    return ret
+
+def rearrange(w,h,ew,eh,data):
+    ret=[]
+    n=w*eh
+    nw=w/ew
+    nh=h/eh
+    for k in range(nh):
+        for i in range(nw):
+            for y in range(eh):
+                for x in range(ew):
+                    ret.append(data[y*w+x+i*ew+k*n])
+    w=ew
+    h=nw*eh*nh
+    return w,h,ret
+
+def getmono(fp,size=None):
+    im = Image.open(fp)
+    if im.mode=='RGBA':
+        ret,ret1=decode2(im.size[0],im.size[1],list(im.getdata()))
+    if size != None:
+        im.thumbnail(size, Image.ANTIALIAS)
+    return im.size,list(im.getdata())
+
+class FileNameListAction(argparse.Action):
+    def __init__(self, option_strings, dest, nargs='+', **kwargs):
+        super(FileNameListAction, self).__init__(option_strings, dest, nargs, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='tanslate png to oled bitmap format')
+    parser.add_argument('-s', '--src',    default=None,         help='srcource directory of input files ')
+    parser.add_argument('-d', '--dest',   default=None,         help='destination directory of output file')
+    parser.add_argument('-m', '--mode',   default=1,            help='mode of decode, default is 0, 0:row, 1: column')
+    parser.add_argument('-p', '--prefix', default='assert_',    help='prefix of generated code')
+    parser.add_argument('-g', '--debug',  action='store_false', help='debug, default is true')
+    parser.add_argument('-l', '--list',   metavar='<filename>', help='need mask filename list, separated by space',action=FileNameListAction)
+    args = parser.parse_args()
+    if args.src is None:
+        sys.stderr.write('you must input the srcource directory of input files\n')
+        sys.exit(0)
+    for name in glob.glob(os.path.join(args.src,'*')):
+        if not name.endswith('.png'):
+            continue
+        im = Image.open(name)
+        w,h=im.size
+        ew,eh=getsize(name,w,h)
+        frames=h/eh
+        name=basename(name)
+        func_name,ext=splitext(basename(name))
+        func_name=func_name.replace('-', '_')
+        if (name in args.list) and (im.mode=='RGBA'):
+            ret=decode2(w,h,list(im.getdata()))
+        else:
+            data = list(im.getdata())
+            ret=decode1(w,h,data)
+        s=gencode(args.prefix+func_name,frames,w,h,ret)
+        print s
